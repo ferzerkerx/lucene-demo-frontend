f0ec4b70d7752f8201da4f5710a23a361527b443
fernandoMontes <fernando@element.in>
2018-03-19 22:31:47 +0100

    - added test

--- src/Service.js
+++ src/Service.js
@@ -1,22 +1,9 @@
+import {checkStatus, parseJSON} from './Utils'
+
 export const Service = (text) => {
-
-    const checkStatus = (response) => {
-        if (response.status >= 200 && response.status < 300) {
-            return response
-        }
-
-        let error = new Error(response.statusText);
-        error.response = response;
-        throw error;
-    };
-
-    const parseJSON = (response) => {
-        return response.json()
-    };
-
     const encodedQuery = encodeURIComponent(text);
     return fetch(`http://localhost:8080/books?keyword=${encodedQuery}`, {credentials: 'include'})
         .then(checkStatus)
         .then(parseJSON)
-        .catch((error)  => console.log('request failed', error));
+        .catch((error)  => console.error('request failed', error));
 };
---
+++ src/Utils.test.js
@@ -0,0 +1,13 @@
+import {checkStatus} from './Utils'
+
+it('should throw error when response is not successful', () => {
+  function check() {
+    checkStatus({status: 400});
+  }
+  expect(check).toThrowErrorMatchingSnapshot();
+})
+
+it('should return successful response', () => {
+  let response = {status: 200}
+  expect(checkStatus(response)).toBe(response);
+});
